7561dda7c87ec4a2131bbfbce485e54b904aee9f
Dima Krasner <dima@dimakrasner.com>
2019-04-25 00:00:57 +0300

    Made the CI script re-run itself through ksh

--- ci.sh
+++ ci.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -xe
 
-jobs=`grep ^processor /proc/cpuinfo | wc -l`
+jobs=`nproc`
 CC=gcc-8 make -j$jobs
 make DESTDIR=dst install
 [ "`./dst/usr/bin/ksh -c 'echo $((1337 * 2))'`" -ne 2674 ] && exit 1
 [ "`./dst/usr/bin/ksh -c 'seq 1337 | sort -rn | head -n 1'`" -ne 1337 ] && exit 1
 make clean
 CC=clang-8 make -j$jobs
+
+if [ "`readlink /proc/$$/exe`" != /tmp/ksh ]
+then
+	cp -f ksh /tmp/
+	make clean
+	exec /tmp/ksh $0
+fi
