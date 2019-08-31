423a52a75d1cc787f16e85cde40a6dac3610bb67
Dima Krasner <dima@dimakrasner.com>
2019-08-31 14:41:59 +0300

    Switched to default gcc and clang versions in CI

--- ci.sh
+++ ci.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -xe
 
 jobs=`nproc`
-CC=gcc-8 make -j$jobs
+make -j$jobs
 make DESTDIR=dst install
 [ "`./dst/usr/bin/ksh -c 'echo $((1337 * 2))'`" -ne 2674 ] && exit 1
 [ "`./dst/usr/bin/ksh -c 'seq 1337 | sort -rn | head -n 1'`" -ne 1337 ] && exit 1
 make clean
-CC=clang-8 make -j$jobs
+CC=clang make -j$jobs
 
 if [ "`readlink /proc/$$/exe`" != /tmp/ksh ]
 then
