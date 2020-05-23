f3ac1d82c62de2b37e3889a537f12db41c5fb6b3
Dima Krasner <dima@dimakrasner.com>
2020-05-23 11:16:00 +0300

    Build with musl in CI

--- ci.sh
+++ ci.sh
@@ -13,3 +13,9 @@ then
 	rm -rf build build-clang dst
 	exec /tmp/ksh -xe $0
 fi
+
+for i in arm-any32-linux-musleabi mips-any32-linux-musl
+do
+	meson --buildtype=release --cross-file=$i build-$i
+	ninja -C build-$i
+done
